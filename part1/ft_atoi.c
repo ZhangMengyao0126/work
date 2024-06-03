807e42c1dad252c99d357f1c3d45260bf48b984b
ZhangMengyao0126 <377922802@qq.com>
2024-06-03 15:42:30 +0300

    atoi,is系列

---
+++ part1/ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzhang <mzhang@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 14:35:09 by mzhang            #+#    #+#             */
+/*   Updated: 2024/06/03 14:36:51 by mzhang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int result;
+	int i;
+	int sign;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\r' ||
+			str[i] == '\t' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * result);
+}
+
+int main(void)
+{
+    char str[] = " -12345abc";
+    int result = ft_atoi(str);
+    printf ("%d ", result);
+    return 0;
+}
