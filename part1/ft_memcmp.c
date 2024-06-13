f7e1d6c198d74a5b698aa9bbf97ac00fb2d6d8a1
ZhangMengyao <377922802@qq.com>
2024-06-13 09:35:17 +0300

    memcmp无header

---
+++ part1/ft_memcmp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int ft_memcmp(void *s1, void *s2, size_t n)
+{
+    unsigned char *s1c = s1;
+    unsigned char *s2c = s2;
+    size_t i;
+    
+    i = 0;
+    while(i++ < n && *(s1c + i) == *(s2c + i));
+    if(i == n)
+        return (0);
+    return(*(s1c + i) - *(s2c + i));
+}
+
+int main()
+{
+    char s1[50] = "Applebanana";
+    char s2[50] = "Applobanana";
+    printf("%d",ft_memcmp(s1,s2,10));
+}
