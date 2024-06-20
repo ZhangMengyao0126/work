b27ff3386e50ca3b014e6eb87e9a4b5fe4ddf90a
ZhangMengyao <377922802@qq.com>
2024-06-20 17:22:12 +0300

    必要函数阶段性完成

---
+++ part1/ft_putendl_fd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void ft_putendl_fd(char *s, int fd)
+{
+    ft_putstr_fd(s, fd);
+    ft_putchar_fd('\n', fd);
+}
+
+int main(void)
+{
+    char *s = "Hello World";
+    ft_putendl_fd(s,1);
+}
---
+++ part1/ft_putstr_fd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+size_t	ft_strlen(const char*str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void ft_putstr_fd(char *s, int fd)
+{
+    write(fd, s, ft_strlen(s));
+}
+
+int main(void)
+{
+    char *s = "Hello World";
+    ft_putstr_fd(s,1);
+}
