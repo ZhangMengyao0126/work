1b5d6979370cf043bf4340ca01df375b2b95fd0b
ZhangMengyao <377922802@qq.com>
2024-06-25 15:10:40 +0300

    QAQ

--- part1/ft_bzero.c
+++ part1/ft_bzero.c
@@ -1,26 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mzhang <mzhang@student.hive.fi>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/06 14:52:36 by mzhang            #+#    #+#             */
-/*   Updated: 2024/06/06 14:52:49 by mzhang           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
-
-/*int main()
-{
-    char str[100] = "Hello World!";
-    ft_bzero(str + 6, 1);
-    printf("After memset(): %s\n", str);
-    return 0;
-}*/
--- part1/ft_memset.c
+++ part1/ft_memset.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mzhang <mzhang@student.hive.fi>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/06 15:25:49 by mzhang            #+#    #+#             */
-/*   Updated: 2024/06/06 15:25:51 by mzhang           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
@@ -26,11 +14,3 @@ void	*ft_memset(void *b, int c, size_t len)
 	}
 	return (b);
 }
-
-/*int main()
-{
-    char str[100] = "Hello World!";
-    ft_memset(str + 6, 'o', 6);
-    printf("After memset(): %s\n", str);
-    return 0;
-}*/
